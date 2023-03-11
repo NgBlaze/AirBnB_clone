c53fbdea52d2062f018d9cf13e76902729d0319d
Precious Kingsley Okolaa <101266877+NgBlaze@users.noreply.github.com>
2023-03-11 12:30:41 +0100

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,16 +1,28 @@
+
 #!/usr/bin/python3
-""" testing User """
+"""Test suite for the User class in models.user"""
 import unittest
-import pep8
+from models.base_model import BaseModel
+
 from models.user import User
 
-class User_testing(unittest.TestCase):
-    """ check BaseModel """
 
-    def testpep8(self):
-        """ testing codestyle """
-        pepstylecode = pep8.StyleGuide(quiet=True)
-        path_user = 'models/user.py'
-        result = pepstylecode.check_files([path_user])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+class TestUser(unittest.TestCase):
+    """Test cases against the User class"""
+
+    def test_attrs_are_class_attrs(self):
+        u = User()
+        # test that it is a class attribute
+        self.assertTrue(hasattr(User, "first_name")
+                        and hasattr(User, "last_name"))
+
+    def test_class_attrs(self):
+        u = User()
+        self.assertIs(type(u.first_name), str)
+        self.assertIs(type(u.last_name), str)
+        self.assertTrue(u.first_name == "")
+        self.assertTrue(u.last_name == "")
+
+    def test_user_is_a_subclass_of_basemodel(self):
+        u = User()
+        self.assertTrue(issubclass(type(u), BaseModel))
